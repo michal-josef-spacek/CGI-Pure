5964c5eb29d17d8c01df6f9d055b0b69ea9a4c2e
Michal Špaček <skim@cpan.org>
2004-09-30 06:59:15 +0000

    Class SCGI::Fast as replace CGI::Fast.

---
+++ Pure/Fast.pm
@@ -0,0 +1,55 @@
+#------------------------------------------------------------------------------
+package SCGI::Fast;
+#------------------------------------------------------------------------------
+# $Id: Fast.pm,v 1.1 2004-09-30 06:59:15 skim Exp $
+
+# Modules.
+use strict;
+use SCGI;
+use FCGI;
+
+# Inheritance.
+@ISA = ('SCGI');
+
+# Version.
+$SCGI::Fast::VERSION='skim_1.05';
+
+# Global variables.
+use vars qw($Ext_Request);
+
+# Workaround for known bug in libfcgi.
+while (($ignore) = each %ENV) { }
+
+# If ENV{'FCGI_SOCKET_PATH'} is specified, we maintain a FCGI Request handle
+# in this package variable.
+BEGIN {
+
+	# If ENV{FCGI_SOCKET_PATH} is given, explicitly open the socket,
+	# and keep the request handle around from which to call Accept().
+	if ($ENV{'FCGI_SOCKET_PATH'}) {
+		my $path = $ENV{'FCGI_SOCKET_PATH'};
+		my $backlog = $ENV{'FCGI_LISTEN_QUEUE'} || 100;
+		my $socket  = FCGI::OpenSocket($path, $backlog);
+		$Ext_Request = FCGI::Request(\*STDIN, \*STDOUT, \*STDERR, 
+					\%ENV, $socket, 1);
+	}
+}
+
+#------------------------------------------------------------------------------
+sub new {
+#------------------------------------------------------------------------------
+# New is slightly different in that it calls FCGI's accept() method.
+
+	my ($self, $initializer, @param) = @_;
+	unless (defined $initializer) {
+		if ($Ext_Request) {
+			return undef unless $Ext_Request->Accept() >= 0;
+		} else {
+			return undef unless FCGI::accept() >= 0;
+		}
+	}
+	return my $self = $self->SUPER::new($initializer, @param);
+}
+# END of new().
+
+1;
