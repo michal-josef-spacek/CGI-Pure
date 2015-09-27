cd1c08c2a9eb6b80cc941236e4565a20ce9fe5af
Michal Špaček <tupinek@gmail.com>
2015-09-27 15:18:30 +0200

    CGI::Pure::Save: Remove TODOs from doc, add examples.

--- Pure/Save.pm
+++ Pure/Save.pm
@@ -88,41 +88,146 @@ CGI::Pure::Save - Common Gateway Interface Class for loading/saving object in fi
 
  use CGI::Pure::Save;
  my $cgi = CGI::Pure::Save->new(%parameters);
- TODO
+ $cgi->save($fh);
+ $cgi->load($fh);
 
 =head1 METHODS
 
 =over 8
 
-=item B<new(%parameters)>
+=item C<new(%parameters)>
 
  Constructor
 
 =over 8
 
-=item * B<cgi_pure>
+=item * C<cgi_pure>
 
- TODO
+ CGI::Pure object.
 
 =back
 
-=item B<load(TODO)>
+=item C<load($fh)>
 
- TODO
+ Load parameters from file.
+ Return undef.
 
-=item B<save(TODO)>
+=item C<save($fh)>
 
- TODO
+ Save parameters to file.
+ Return undef.
 
 =back
 
 =head1 ERRORS
 
-TODO
+ new():
+         CGI::Pure object doesn't define.
+         From Class::Utils::set_params():
+                 Unknown parameter '%s'.
 
-=head1 EXAMPLE
+ load()
+         Invalid filehandle.
 
-TODO
+ save()
+         Invalid filehandle.
+
+=head1 EXAMPLE1
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use CGI::Pure;
+ use CGI::Pure::Save;
+ use File::Temp qw(tempfile);
+ use Perl6::Slurp qw(slurp);
+
+ # Temporary file.
+ my ($tempfile_fh, $tempfile) = tempfile();
+
+ # Query string.
+ my $query_string = 'par1=val1;par1=val2;par2=value';
+
+ # CGI::Pure Object.
+ my $cgi = CGI::Pure->new(
+ 	'init' => $query_string,
+ );
+
+ # CGI::Pure::Save object.
+ my $save = CGI::Pure::Save->new(
+ 	'cgi_pure' => $cgi,
+ );
+
+ # Save.
+ $save->save($tempfile_fh);
+ close $tempfile_fh;
+ 
+ # Print file.
+ print slurp($tempfile);
+
+ # Clean temp file.
+ unlink $tempfile;
+
+ # Output:
+ # par1=val1
+ # par1=val2
+ # par2=value
+ # =
+
+=head1 EXAMPLE2
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use CGI::Pure;
+ use CGI::Pure::Save;
+ use IO::Barf qw(barf);
+ use File::Temp qw(tempfile);
+ use File::Slurp qw(write_file);
+
+ # Temporary file.
+ my ($tempfile_fh, $tempfile) = tempfile();
+
+ # CGI::Pure data.
+ my $cgi_pure_data = <<'END';
+ par1=val1
+ par1=val2
+ par2=value
+ =
+ END
+
+ # Create file.
+ barf($tempfile_fh, $cgi_pure_data);
+ close $tempfile_fh;
+
+ # CGI::Pure Object.
+ my $cgi = CGI::Pure->new;
+
+ # CGI::Pure::Save object.
+ my $save = CGI::Pure::Save->new(
+ 	'cgi_pure' => $cgi,
+ );
+
+ # Load.
+ open $tempfile_fh, '<', $tempfile;
+ $save->load($tempfile_fh);
+ close $tempfile_fh;
+
+ # Print out.
+ foreach my $param_key ($cgi->param) {
+ 	print "Param '$param_key': ".join(' ', $cgi->param($param_key))."\n";
+ }
+
+ # Clean temp file.
+ unlink $tempfile;
+
+ # Output:
+ # Param 'par1': val1 val2
+ # Param 'par2': value
 
 =head1 DEPENDENCIES
 
